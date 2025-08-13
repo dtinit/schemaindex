70c96d17cf0ac4e961fd91c875f19d43f478efee
alexbainter <alexbainter@gmail.com>
2025-08-13 13:47:52 -0500

    Setup admin for models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.admin.decorators import register
+from .models import Schema, SchemaVersion, SchemaRef, DocumentationItem
 
-# Register your models here.
+
+@register(Schema)
+class SchemaAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+@register(SchemaVersion)
+class SchemaVersionAdmin(admin.ModelAdmin):
+    list_display = ['schema', 'name', 'published_at']
+
+
+@register(SchemaRef)
+class SchemaRefAdmin(admin.ModelAdmin):
+    list_display = ['url', 'format', 'schema_version']
+
+
+@register(DocumentationItem)
+class DocumentationItem(admin.ModelAdmin):
+    list_display = ['name', 'url', 'format', 'schema', 'schema_version']
