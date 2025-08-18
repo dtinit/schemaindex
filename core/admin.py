d5c20d4118acd9884f5e0f0074853fc1716c02f5
Alex Bainter <6510436+alexbainter@users.noreply.github.com>
2025-08-18 13:34:31 -0500

    Initial data models (#4)
    * Set up initial data models
    
    * Setup admin for models
    
    * Refactor models to separate SchemaDocumentationItems from SchemaVersionDocumentationItems
    
    * Remove SchemaVersions

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.admin.decorators import register
+from .models import Schema, SchemaRef, DocumentationItem
 
-# Register your models here.
+
+@register(Schema)
+class SchemaAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+@register(SchemaRef)
+class SchemaRefAdmin(admin.ModelAdmin):
+    list_display = ['schema', 'url']
+
+
+@register(DocumentationItem)
+class DocumentationItemAdmin(admin.ModelAdmin):
+    list_display = ['schema', 'name', 'url']
+
--- core/models.py
+++ core/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.db.models import Q
+from django.contrib.auth.models import User
 
-# Create your models here.
+class BaseModel(models.Model):
+    class Meta:
+        abstract = True
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, on_delete=models.RESTRICT)
+
+    @classmethod
+    def create(cls, created_by):
+        return cls(created_by=created_by)
+
+
+class Schema(BaseModel):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+    
+
+class ReferenceItem(BaseModel):
+    class Meta:
+        abstract = True
+
+    url = models.URLField()
+
+    def __str__(self):
+        return self.url
+
+
+class SchemaRef(ReferenceItem):
+    schema = models.ForeignKey(Schema, on_delete=models.CASCADE)
+
+
+class DocumentationItem(ReferenceItem):
+    name = models.CharField(max_length=300)
+    description = models.TextField(blank=True, null=True)
+    schema = models.ForeignKey(Schema, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
