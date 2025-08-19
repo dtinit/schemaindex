7b24e2223867b973f1f048c0639372364a180b50
alexbainter <alexbainter@gmail.com>
2025-08-19 15:00:37 -0500

    Add homepage

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Schema
 
-# Create your views here.
+def index(request):
+    schemas = (Schema.objects
+               .prefetch_related("schemaref_set")
+               .filter(schemaref__isnull=False)
+               .order_by("name").all())
+    return render(request, "core/index.html", {
+        "schemas": schemas
+    })
