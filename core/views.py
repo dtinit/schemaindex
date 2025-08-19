7a26c5f6503c37850b45308e16d114d49a3b65dc
Alex Bainter <6510436+alexbainter@users.noreply.github.com>
2025-08-19 17:45:04 -0500

    Basic homepage with searchable schema list (#8)
    * Add homepage
    
    * Add search

--- core/views.py
+++ core/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.db.models import Count
+from .models import Schema
 
-# Create your views here.
+MAX_SCHEMA_RESULT_COUNT = 30
+
+def index(request):
+
+    defined_schemas = (
+        Schema.objects
+        .prefetch_related("schemaref_set")
+        .filter(schemaref__isnull=False)
+    )
+
+    search_query = request.GET.get('search_query', None)
+    results = defined_schemas.filter(name__icontains=search_query) if search_query else defined_schemas
+
+    return render(request, "core/index.html", {
+        "total_schema_count": defined_schemas.count(),
+        "schemas": results.order_by("name")[:MAX_SCHEMA_RESULT_COUNT]
+    })
