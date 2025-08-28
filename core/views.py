9d55f6b6df2acb6290c6a2e158e3cf7995eaf8f1
alexbainter <alexbainter@gmail.com>
2025-08-28 11:37:38 -0500

    Remove leftover unused import

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404
 from django.db.models import Count
 from .models import Schema
 
