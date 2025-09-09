e23a1e06600830eeae19435601474744880887fc
Alex Bainter <6510436+alexbainter@users.noreply.github.com>
2025-09-09 11:59:54 -0500

    Account profile page (#18)
    * Add account profile page
    
    * Add empty state to the account profile

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("schemas/<int:schema_id>", views.schema_detail, name="schema_detail")
+    path("schemas/<int:schema_id>/", views.schema_detail, name="schema_detail"),
+    path("account/profile/", views.account_profile, name="account_profile")
 ]
 
