4e1cb6b207dbcd1c138acebd5cb8cf7eeb8c8414
Alex Bainter <6510436+alexbainter@users.noreply.github.com>
2025-09-17 14:26:42 -0500

    User Schema CRUD forms (#22)
    * Add form for creating and editing schemas
    
    * Add schema deletion

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,9 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("schemas/<int:schema_id>/", views.schema_detail, name="schema_detail"),
-    path("account/profile/", views.account_profile, name="account_profile")
+    path("account/profile/", views.account_profile, name="account_profile"),
+    path("manage/schema/<int:schema_id>", views.manage_schema, name="manage_schema"),
+    path("manage/schema/new", views.manage_schema, name="manage_schema_new"),
+    path("manage/schema/<int:schema_id>/delete", views.manage_schema_delete, name="manage_schema_delete")
 ]
 
