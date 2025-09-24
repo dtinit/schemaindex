b71d48c09ab9b5f29d28a6711f1f7d04c8097ff1
alexbainter <alexbainter@gmail.com>
2025-09-24 14:29:34 -0500

    Implement URL validation for new schemas

--- core/forms.py
+++ core/forms.py
@@ -1,12 +1,72 @@
 from django import forms
+from django.core.exceptions import ValidationError
+import requests
+from pygments.lexers import get_lexer_for_filename
+from pygments.util import ClassNotFound
 from .models import DocumentationItem
+
+'''
+This is currently just a list of languages supported
+by our syntax highlighter, Highlight.js, *without plaintext.*
+You can regenerate this list by loading up the site,
+opening a JS REPL in the browser's dev tools,
+and executing `hljs.listLanguanges()`.
+
+Note that the actual allowlist is an intersection
+of this list and the lexers from pygments.
+'''
+SPECIFICATION_LANGUAGE_ALLOWLIST = [
+"bash","c","cpp","csharp","css","diff","go","graphql","ini","java","javascript","json","kotlin","less","lua","makefile","markdown","objectivec","perl","php","php-template","python","python-repl","r","ruby","rust","scss","shell","sql","swift","typescript","vbnet","wasm","xml","yaml"
+]
 
 class SchemaForm(forms.Form):
     name = forms.CharField(label="Schema name", max_length=200)
     reference_url = forms.URLField(label="Schema definition URL")
     readme_url = forms.URLField(label="Schema README URL")
     readme_format = forms.ChoiceField(
-        choices=list(DocumentationItem.DocumentationItemFormat.choices) + [('', 'Other')],
+        choices=DocumentationItem.DocumentationItemFormat.choices,
         required=False,
         label="README format"
     )
+
+    def _clean_url(self, url_field_name, language_allowlist):
+        data = self.cleaned_data[url_field_name]
+        try:
+            response = requests.get(data)
+        except requests.exceptions.RequestException:
+            raise ValidationError("The provided URL could not be reached")
+
+        if response.status_code != requests.codes.ok:
+            raise ValidationError("The provided URL returned an invalid status code")
+
+        if not response.text:
+            raise ValidationError("The provided URL has no text content")
+
+        # Use pygments to verify the language from the filename
+        try:
+            lexer = get_lexer_for_filename(data)
+        except ClassNotFound:
+            if not "plaintext" in language_allowlist:
+                raise ValidationError("The provided URL does not have a supported file extension")
+            else:
+                # If we don't have a match but plaintext is allowed, we'll just treat it as plaintext
+                return [data, "plaintext"]
+
+        matched_language = next(
+            (alias for alias in language_allowlist if alias in lexer.aliases),
+            "plaintext" if "plaintext" in language_allowlist else None
+        )
+        if not matched_language:
+            raise ValidationError("The text content at the provided URL is not in a supported format")
+
+        return [data, matched_language]
+
+    def clean_reference_url(self):
+        [data, matched_language] = self._clean_url('reference_url', language_allowlist=SPECIFICATION_LANGUAGE_ALLOWLIST)
+        return data
+
+    def clean_readme_url(self):
+        [data, matched_language] = self._clean_url('readme_url', language_allowlist=DocumentationItem.DocumentationItemFormat)
+        self.readme_format = matched_language
+        return data
+
