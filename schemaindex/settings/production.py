71c27ddee8e74741a594e9b5fe84dd49c85be117
alexbainter <alexbainter@gmail.com>
2025-10-14 14:03:51 -0500

    Add email settings for staging and production

--- schemaindex/settings/production.py
+++ schemaindex/settings/production.py
@@ -73,3 +73,11 @@ LOGGING = {
         },
     },
 }
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = "noreply@dtinit.org"
+EMAIL_HOST_PASSWORD = env.str('EMAIL_HOST_PASSWORD')
