b22f20c55c4e190493bc2d231ecda7a10fd684a3
Alex Bainter <6510436+alexbainter@users.noreply.github.com>
2025-11-18 17:54:37 -0600

    Ensure highlight.js is always called (#137)

--- core/static/js/schema-detail.js
+++ core/static/js/schema-detail.js
@@ -1,20 +1,31 @@
 (() => {
-  const hljsScriptSrc = document.getElementById('hljs-src');
-  if (!hljsScriptSrc) {
-    return;
-  }
-  const hljsScriptSrcLoaded = new Promise((resolve) => {
-    hljsScriptSrc.addEventListener('load', resolve);
-  });
-
-  const domContentLoaded = new Promise((resolve) => {
-    document.addEventListener('DOMContentLoaded', resolve);
-  });
-
-  Promise.all([hljsScriptSrcLoaded, domContentLoaded]).then(() => {
-    // TODO: figure out why we can't declare this in globals.d.ts
-    /** @type window & {hljs: {highlightAll: () => void }} */ (
-      window
-    ).hljs.highlightAll();
+  document.addEventListener('DOMContentLoaded', () => {
+    /**
+     * @import { HLJSApi } from 'highlight.js';
+     * @type Promise<HLJSApi>
+     */
+    const hljsPromise = new Promise((resolve, reject) => {
+      if (window.hljs) {
+        resolve(window.hljs);
+        return;
+      }
+      const hljsScriptSrc = document.getElementById('hljs-src');
+      if (!hljsScriptSrc) {
+        reject(new Error('A Highlight.js script tag was not found.'));
+        return;
+      }
+      hljsScriptSrc.addEventListener('load', () => {
+        if (!window.hljs) {
+          reject(new Error('Highlight.js failed to initialize.'));
+          return;
+        }
+        resolve(window.hljs);
+      });
+    });
+    hljsPromise
+      .then((hljs) => hljs.highlightAll())
+      .catch((err) => {
+        console.error(err);
+      });
   });
 })();
