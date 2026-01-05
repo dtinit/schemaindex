ce9598b5c4e0b3e15705f549c5fa2e36cc966d83
Alex Bainter <6510436+alexbainter@users.noreply.github.com>
2026-01-05 14:09:20 -0600

    Pull reference item content from raw github URLs when available (#168)

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,5 +1,5 @@
 import pytest
-
+import requests_mock
 from core.models import Schema, SchemaRef
 from factories import UserFactory
 
@@ -52,3 +52,13 @@ def test_reference_item_github_url_info_converts_repo_url_to_raw(repo_url, raw_u
 def test_reference_item_github_url_info_converts_raw_url_to_repo(repo_url, raw_url):
     schema_ref = SchemaRef(url=raw_url)
     assert schema_ref.url_provider_info.repo_url == repo_url
+
+
+def test_reference_item_content_is_fetched_from_raw_url_if_available():
+    schema_ref = SchemaRef(url="https://github.com/userorg/reponame/blob/branch/path/to/file.json")
+    mock_content = '{"MOCK_CONTENT":true}'
+    # Make sure the URL we expect to be fetched isn't the one we provided
+    assert schema_ref.url != schema_ref.url_provider_info.raw_url
+    with requests_mock.Mocker() as m:
+        m.get(schema_ref.url_provider_info.raw_url, text=mock_content)
+        assert schema_ref.get_content() == mock_content
