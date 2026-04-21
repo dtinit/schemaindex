454a06ded03879f37d03fa6762a6b9f53fc90333
alexbainter <alexbainter@gmail.com>
2026-04-21 16:06:09 -0500

    Add id_value to SchemaRef model

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from django.core.cache import cache
+import requests_mock as requests_mock_lib
 
 
 @pytest.fixture(autouse=True)
@@ -11,3 +12,17 @@ def clear_cache():
     cache.clear()
     yield
     cache.clear()
+
+
+@pytest.fixture(autouse=True)
+def fallback_get_request_mock(requests_mock):
+    """
+    Catch-all fallback for GET requsets
+    """
+    # This matches any URL (http/https) and any method (GET/POST/etc.)
+    requests_mock.get(
+        requests_mock_lib.ANY, 
+        json={'message': 'Default fallback response'},
+        status_code=200
+    )
+    return requests_mock
