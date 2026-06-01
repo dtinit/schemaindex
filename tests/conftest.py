73aad2733ec3fdd83ff34243ce13761728bce09b
alexbainter <alexbainter@gmail.com>
2026-06-01 15:33:16 -0500

    Exempt API views from CSRF checks

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from django.core.cache import cache
+from django.test import Client
 import requests_mock as requests_mock_lib
 from factories import ProfileFactory
 
@@ -30,9 +31,19 @@ def fallback_get_request_mock(requests_mock):
 
 
 @pytest.fixture
-def api_client(client, db):
+def api_client(db):
     profile = ProfileFactory.create()
     api_key = profile.set_new_api_key()
+    # Django requires CSRF tokens for "unsafe" HTTP methods
+    # (POST/PUT/DELETE/etc) by default. To help verify that
+    # our API views are exempt from CSRF checks as intended,
+    # here we enable CSRF checks in the Django test client
+    # used for API testing. Any API tests which use this
+    # test client to test an API view that isn't exempt from CSRF
+    # will fail, alerting developers of the problem.
+    # Use the @csrf_exempt decorator for API views with "unsafe"
+    # HTTP methods.
+    client = Client(enforce_csrf_checks=True)
     client.defaults["HTTP_X_API_KEY"] = api_key
     client.user = profile.user
     return client
